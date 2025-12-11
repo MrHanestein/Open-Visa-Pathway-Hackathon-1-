72eb4a255d9077af0e5901e005a501efe732a063
Mr Hanestein <giddydanny002@gmail.com>
2025-12-11 17:21:51 +0100

    I added an open ai intergration alongside rate limits using 6 dollars

---
+++ test_openai.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+
+client = OpenAI() #Open AI client auto reader
+
+def main():
+    response = client.responses.create(
+        model = "gpt-5-nano",
+        input = "Say hello to Michael from Nigeria who dreams of studying in the UK.",
+        max_output_tokens=50
+    )
+    print(response.output_text)
+    print("Test file for OpenAI integration.")
+    
+if __name__ == "__main__":
+    main()
---
+++ test_streamlit.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Open-visa-way test file")
+st.write("Nigeria and india international students can apply for open visa way")
